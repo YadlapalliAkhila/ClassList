bd406b4a4313f103b1af9bc87f34d4d4fa1b5894
yadlapalliakhila <yesakhila114@gmail.com>
2021-08-30 18:22:26 -0500

    First Git Commit

---
+++ Sample.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Sample{
+Map<String, String> Hash = new HashMap<String, String>();
+
+public Sample()
+{
+Hash.put("Bob","A");
+
+Hash.put("Mary","C");
+Hash.put("Sarah", "B");
+
+Hash.put("Philip","A");
+
+Hash.put("Greg","F");
+}
+public void printHash()
+{
+Iterator<Entry<String, String>> entries = Hash.entrySet().iterator();
+while (entries.hasNext())
+{ 
+//sample
+  Entry<String,String> entry = entries.next();
+  System.out.println("key = " + entry.getKey()+ " , Value " + entry.getValue());
+  
+}
+} 
+
+
+public void printHash2()
+{
+
+for (Map.Entry<String, String> entry: Hash.entrySet()) 
+{
+System.out.println("Key = " + entry.getKey() + ",Value " + entry.getValue());
+}
+
+for (String key: Hash.keySet()) 
+{
+System.out.println("key = " +key);
+}
+for (String value : Hash.values())
+{
+System.out.println("Value = " +value);
+}
+}
+public static void main(String[] args)
+{ 
+Sample obj = new Sample(); 
+obj.printHash();
+obj.printHash2();
+}
+}
